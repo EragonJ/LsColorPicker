6960a1ddb002d81758251950c499573e747f2b1c
EragonJ <jack.xxlong@gmail.com>
2010-09-11 21:35:59 +0800

    Add some comments for readibility

--- picker.pl
+++ picker.pl
@@ -1,54 +1,63 @@
 #!usr/bin/perl -w
+
+# Use the package
 use Term::ANSIColor;
 use Term::ANSIColor qw(:constants);
 
-my @questions = qw{directory symbolic_Link socket pipe executable block_special character_special executable_with_setuid_bit_set executable_with_setgid_bit_set directory_writable_to_others_with_sticky_bit directory_writable_to_others_without_sticky_bit};
+# These are ls options
+my @options = qw{directory symbolic_Link socket pipe executable block_special character_special executable_with_setuid_bit_set executable_with_setgid_bit_set directory_writable_to_others_with_sticky_bit directory_writable_to_others_without_sticky_bit};
 
+# Get the foreground color
 my $list = qq{BLACK,RED,GREEN,YELLOW,BLUE,MAGENTA,CYAN,WHITE,BOLD BLACK,BOLD RED,BOLD GREEN,BOLD YELLOW,BOLD BLUE,BOLD MAGENTA,BOLD CYAN,BOLD WHITE};
-
 my @foreground = split(',',$list);
+
+# Get the background color
 my @background = qw{ON_BLACK ON_RED ON_GREEN ON_YELLOW ON_BLUE ON_MAGENTA ON_CYAN ON_WHITE};
+
+# User settings
 my $setting = '';
 
-my $qc=0;
-my $ans;
+my $result;
 
-foreach my $q (@questions)
+foreach my $option (@options)
 {
 	do{
-		local $Term::ANSIColor::AUTORESET = 1;
-		print "Set your own '".$q."' color::\n";
 
-		my $fgc=0;
-		my $bgc=0;
+		local $Term::ANSIColor::AUTORESET = 1;
+		print "Set your own '".$option."' color::\n";
+
+		my $fgCount=0;
+		my $bgCount=0;
+
+    # Display the color options for each ls option
 		foreach my $fg (@foreground)
 		{
 			foreach my $bg (@background)
 			{
 				local $Term::ANSIColor::AUTORESET = 1;
 				#format
-				print "0" if $fgc < 10; 
-				print $fgc.$bgc.")";
+				print "0" if $fgCount < 10; 
+				print $fgCount.$bgCount.")";
 				print colored("TEST",$fg.' '.$bg);
 				print " ";
-				$bgc++;
+				$bgCount++;
 			}
 			print "\n";
-			$bgc=0;
-			$fgc++;
+			$bgCount=0;
+			$fgCount++;
 		}
-		print "Plz type the number shown on the top.\n";
-		print ">>";
 
-		#input
-		$ans = <>;
-		chomp($ans);
+		print "Plz type the number shown on the top.\n>> ";
+
+		# Get the result from the user
+		$result = <>;
+		chomp($result);
 		system("clear");
-		print "Your choise is not correct! plz try again\n" if(length($ans)!=3);
+		print "Your choise is not correct! plz try again\n" if(length($result)!=3);
 
-	}while(length($ans)!=3);
+	}while(length($result)!=3);
 
-	assign(substr($ans,0,2),substr($ans,2,1));
+	assign(substr($result,0,2),substr($result,2,1));
 	print "Your setting: '".$setting."'\n";
 }
 
@@ -61,14 +70,14 @@ sub assign
 	my $fg;
 	my $bg;
 
-	#Default value white foreground with black background
+	# Default value white foreground with black background
 	$l = "07" if ($l gt "15");
 	$r = "0"  if ($r gt "7");
 
 	$left  = int($l);
 	$right = int($r); 
 	
-	#for foreground color
+	# For foreground color
 	$fg = 'a' if $left == 0; 
 	$fg = 'b' if $left == 1; 
 	$fg = 'c' if $left == 2; 
@@ -101,7 +110,7 @@ sub assign
 
 sub fileIO
 {
-	my ($set) = @_;
+  my ($set) = @_;
   my $path  = $ENV{HOME}."/.profile";
 	my $check = 0;
   my $all;
